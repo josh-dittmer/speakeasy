7395bab78729afb61cf100ebfa9448cdf4faceeb
Josh Dittmer <joshua.dittmer@sjsu.edu>
2024-10-23 00:36:02 -0700

    infinite query for messages

--- frontend/src/components/chat_area.tsx
+++ frontend/src/components/chat_area.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import { getChannelData } from '@/lib/requests';
-import { keepPreviousData, QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query';
+import { keepPreviousData, QueryClient, QueryClientProvider, useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import { MessageArrayT, MessageT, UserArrayT, UserT } from 'models';
 import Image from 'next/image';
-import { createRef, useEffect, useRef, useState } from 'react';
+import { createRef, Fragment, useEffect, useRef, useState } from 'react';
 
 const queryClient = new QueryClient();
 
@@ -37,65 +37,70 @@ function ChatArea({ channelId, users }: { channelId: string, users: UserArrayT }
     const userMap = new Map<string, UserT>();
     users.map((user) => {
         userMap.set(user.userId, user);
-    })
+    });
 
-    const [prevMessages, setPrevMessages] = useState<MessageArrayT>([]);
-    const [currMessages, setCurrMessages] = useState<MessageArrayT>([]);
-    const [nextMessages, setNextMessages] = useState<MessageArrayT>([]);
-
-    const [messages, setMessages] = useState<MessageArrayT>([]);
-
-    const [page, setPage] = useState<number>(0);
-    const [maxPage, setMaxPage] = useState<number>(Number.MAX_SAFE_INTEGER);
-
-    const { isPending, error, data } = useQuery({
-        queryKey: [channelId, page],
-        queryFn: () => getChannelData(channelId, page),
-        placeholderData: keepPreviousData
+    const fetchMessages = async ({ pageParam }: { pageParam: any }) => await getChannelData(channelId, pageParam);
+    const { data, error, fetchNextPage, isLoading } = useInfiniteQuery({
+        queryKey: [channelId],
+        queryFn: fetchMessages,
+        select: (data) => ({
+            pages: [...data.pages].reverse(),
+            pageParams: [...data.pageParams].reverse(),
+        }),
+        initialPageParam: 0,
+        getNextPageParam: (lastPage, allPages, lastPageParam) => {
+            if (lastPage.messages.length === 0) return undefined;
+            return lastPageParam + 1;
+        },
+        getPreviousPageParam: (firstPage, allPages, firstPageParam) => {
+            if (firstPageParam <= 1) return undefined;
+            return firstPageParam - 1;
+        },
+        //placeholderData: keepPreviousData
     });
 
     const onMessagesScrolled = (e: any) => {
-        if (chatAreaRef.current?.scrollTop === 0 && page + 1 <= maxPage) {
-            setPage((old) => old + 1);
-        } else if (chatAreaRef.current?.scrollTop === chatAreaRef?.current?.scrollHeight && page > 0) {
-            //setPage((old) => old - 1);
+        if (chatAreaRef.current?.scrollTop === 0) {
+            fetchNextPage();
+        } else if (chatAreaRef.current?.scrollTop === chatAreaRef?.current?.scrollHeight) {
+            
         }
     };
 
     const chatAreaRef = createRef<HTMLDivElement>();
     const prevScrollHeightRef = useRef<number>(0);
 
-    // add new messages to screen
-    useEffect(() => {
-        if (data?.messages) {
-            // when server returns empty message array, the max page has been reached
-            if (data.messages.length === 0 && page !== 0) {
-                setMaxPage(page - 1);
-                //setPage((old) => old - 1);
-            }
-
-            setMessages((old) => [...data?.messages, ...old]);
-        }
-    }, [data?.messages]);
-
     // scroll to appropriate position when new messages are loaded
     useEffect(() => {
-        if (chatAreaRef.current ) {
+        if (chatAreaRef.current) {
             const newHeight = chatAreaRef.current.scrollHeight;
             chatAreaRef.current.scrollTop = newHeight - prevScrollHeightRef.current;
             prevScrollHeightRef.current = chatAreaRef.current.scrollHeight;
         }
-    }, [messages]);
+    }, [data?.pages]);
 
-    if (isPending) {
+    // scroll to bottom on intial load in case messages are cached
+    useEffect(() => {
+        if (chatAreaRef.current) {
+            chatAreaRef.current.scrollTop = chatAreaRef.current.scrollHeight;
+            // set visibile after scrolling to avoid ugly jump
+            chatAreaRef.current.style.visibility = 'visible';
+        }
+    }, [chatAreaRef]);
+
+    if (isLoading) {
         return (
-            <p>Loading...</p>
+            <div className="relative h-screen grow flex justify-center items-center">
+                <Image src="/img/loading.gif" width={100} height={100} alt="Loading" />
+            </div>
         )
     }
     
     if (error) {
         return (
-            <p>Error</p>
+            <div className="relative h-screen grow flex justify-center items-center">
+                <p className="text-6xl text-slate-500">:(</p>
+            </div>
         )
     }
 
@@ -103,20 +108,25 @@ function ChatArea({ channelId, users }: { channelId: string, users: UserArrayT }
         <div className="relative h-screen grow">
             <div className="absolute z-10 h-[48px] w-full bg-white flex items-center p-3 shadow-b">
                 <p className="mr-2 text-gray-500 text-xl font-bold">T</p>
-                <p className="text-lg text-gray-600">{data.channel.name}</p>
+                <p className="text-lg text-gray-600">{data?.pages[0]?.channel.name}</p>
             </div>
-            <div className="absolute bottom-[70px] max-h-full w-full overflow-y-scroll w-auto" ref={chatAreaRef} onScroll={(e) => onMessagesScrolled(e)}>
+            <div style={{ visibility: 'hidden' }} className="absolute bottom-[70px] max-h-full w-full overflow-y-scroll w-auto" ref={chatAreaRef} onScroll={(e) => onMessagesScrolled(e)}>
                 <div>
                     <div className="h-[118px] mb-2"></div>
-                    {messages.map((message, index) => {
-                        return <ChatMessage key={index} message={message} user={userMap.get(message.userId)} />
-                    })}
+                    {data?.pages.map((page, index) => (
+                        <Fragment key={index}>
+                            {page.messages.map((message) => {
+                                return <ChatMessage key={message.messageId} message={message} user={userMap.get(message.userId)} />
+                            })}
+                        </Fragment>
+
+                    ))}
                 </div>
             </div>
             <div className="flex justify-center items-center absolute bottom-0 h-[70px] w-full p-3">
                 <input 
                     type="text" 
-                    placeholder={`Send a message to ${data.channel.name}...`} 
+                    placeholder={`Send a message to ${data?.pages[0]?.channel.name}...`} 
                     className="w-full bg-slate-100 p-3 rounded focus:outline-none"    
                 />
             </div>
